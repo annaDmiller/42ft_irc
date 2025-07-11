0c5dcecdfe034999b2ee4726eac230fbf3123325
Anna Melnikova <amelniko@student.42.fr>
2025-07-11 21:43:17 +0200

    Adding macros for replies and adding values for global vars

--- includes/Macros.hpp
+++ includes/Macros.hpp
@@ -2,6 +2,11 @@
 
 # define SOCKMAXCONN 10 //-> maximum number of connections to socket at a time
 # define TERMIN "\r\n" //-> line termination in IRC (based on RFC 2812)
+# define SERVERNAME "FT_IRC"
+# define VERSION "ver.1.0"
+# define CREATEDDATE ""
+# define USERMODES ""
+# define CHANNELMODES "iklot"
 
 //Numeric replies for errors
 # define ERR_NEEDMOREPARAMS(nick, cmd) (": 461 " + nick + " " + cmd + " :Not enough parameters" + TERMIN)
@@ -14,6 +19,6 @@
 
 //Replies to the client
 # define RPL_WELCOME(nick, user, host) (": 001 " + nick + " :Welcome to the Internet Relay Network " + nick + "!" + user + "@" + host + TERMIN)
-# define RPL_YOURHOST 002
-# define RPL_CREATED 003
-# define RPL_MYINFO 004
+# define RPL_YOURHOST(nick, servername, version) (": 002 " + nick + " :Your host is " + servername + ", running version " + version + TERMIN)
+# define RPL_CREATED(nick, date) (": 003 " + nick + " :This server was created " + date + TERMIN)
+# define RPL_MYINFO(nick, servername, version, user_modes, channel_modes) (": 004 " + nick + " " + servername + " " + version + " " + user_modes + " " + channel_modes + TERMIN)
