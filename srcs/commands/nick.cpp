460043da77b12da7e62e023b7c9de4beb4e1ef73
student <student.42.fr>
2025-09-01 19:51:05 +0200

    checkDupNicknamesOnServer update

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -63,7 +63,7 @@ bool Server::checkDupNicknamesOnServer(std::string& nick)
     std::string upper_nick, tmp_nick;
 
     upper_nick = toUpperString(nick);
-    if (upper_nick == BOT_NICK)
+    if (upper_nick == toUpperString(BOT_NICK))
         return(false);
     for (size_t ind = 0; ind < this->_clients.size(); ind++)
     {
