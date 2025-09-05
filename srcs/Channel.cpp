2a4c1faf319eb140da74a898dd6930706e31fa65
student <student.42.fr>
2025-09-05 19:36:46 +0200

    removeMember fix

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -166,9 +166,9 @@ void Channel::removeMember(const int& client_fd, Server& server)
             {
                 bot_fd = it->second->getFD();
                 it->second->leaveChannel(channel_name);
+                this->_members.erase(bot_fd);
             }
         }
-        this->_members.erase(bot_fd);
         if (this->_members.empty()) {
             server.deleteChannel(channel_name);
             return ;
