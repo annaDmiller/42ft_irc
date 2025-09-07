c759707c4343c525f99de470ce3ef91e51470afc
student <student.42.fr>
2025-09-07 20:18:35 +0200

    mode command +k update

--- srcs/commands/mode.cpp
+++ srcs/commands/mode.cpp
@@ -146,6 +146,13 @@ std::string Server::modeHandlingChannel(Client& client, Channel& channel,
                 if (isAdding && ind_param < params.size())
                         pass = params[ind_param++];
 
+				if (isAdding && ind_param >= params.size())
+				{
+					err_message = ERR_NEEDMOREPARAMS(client.getNick(), MODE);
+					client.appendSendBuffer(err_message);
+					return (std::string());
+				}
+
                 if (channel.handleKey(isAdding, pass, client))
                 {
                     modes_for_message.push_back('k');
